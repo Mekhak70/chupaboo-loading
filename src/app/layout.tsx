adbcbd7e00a9579aad59ef80076a003dde8a988c
erik <khechumyanerik1@gmail.com>
2025-10-14 18:04:50 +0400

    add icon and

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,31 @@ import Footer from "@/components/Footer";
 export const metadata: Metadata = {
   title: "Chupaboo",
   description: "Coming soon.",
+  icons: {
+    icon: "/logo.png",
+  },
+  openGraph: {
+    title: "Քո կայքի անունը",
+    description: "Coming soon.",
+    url: "https://www.chupaboo.com/",
+    siteName: "Chupaboo",
+    images: [
+      {
+        url: "/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Preview image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "https://www.chupaboo.com/",
+    description: "Coming soon.",
+    images: ["/logo.png"],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
